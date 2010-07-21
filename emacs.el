0b2d7dded0fee3dbb2f8539f8891ca67ff6d507a
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-07-21 16:39:54 +0200

    Use nxml-mode for html files.

--- emacs.el
+++ emacs.el
@@ -230,6 +230,7 @@
 (add-to-list 'auto-mode-alist '("\\.pt$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.cpt$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.zcml$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . nxml-mode))
 
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.rdf$" . nxml-mode))
